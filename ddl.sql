a9199a000991bfd5cbd6d4ea4f58cb1512cc66da
PI-KA-CHU <529709737@qq.com>
2020-07-01 00:22:59 +0800

    finish task

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,47 @@
 -- **Database level:**
 -- Display all the database
+show databases;
 
 -- Enter a certain database
+use mysql;
 
 -- Create a database
+create database `student`;
 
 -- Create the database of the designated character set
+create database `student` default character set utf8mb4 collate utf8mb4_unicode_ci;
 
 -- Display the creation information fo the database
+show create database `student`;
 
 -- Revise the codes of the database
+-- 修改数据库字符集为 utf8
+alter database `student` default character set utf8;
 
 -- Delete a database
+drop database `student`;
 
 -- **Table level**
 -- Revise table name
+alter table `student` rename t_student;
 
 -- Revise the field's data type
+alter table `student` modify `age` varchar(255) not null;
 
 -- Revise field name
+alter table `student` change `sex` `gender` varchar(255) not null;
 
 -- Add field
+alter table `student` add column `date` datetime not null; 
 
 -- Delete field
+alter table `student` drop column date;
 
 -- Revise the table's storage engine
+alter table `student` engine=innodb;
 
 -- Delete the table's foreign key restriant
+alter table `student` drop foreign key foreignKeyName
 
 -- Delete a table
+drop table `student`; 
--- dml.sql
+++ dml.sql
@@ -1,7 +1,11 @@
 -- Insert record
+insert into `student` (`id`, `name`, `age`, `sex`) values ('003', 'Colin', 18, 'male');
 
 -- Revise record
+update `student` set `age` = `10` where `id` = '001';
 
 --  Delete record
+delete from `student` where `id` = `001`;
 
 -- Search record
+select * from `student` where `id` = '001';
